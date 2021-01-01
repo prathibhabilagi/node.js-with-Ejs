1ca8fe6b7a93b12e27ccd4e732acafe47070f79f
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-01-01 20:06:14 +0530

    Creating Dynamic page using Handlers

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,18 +5,28 @@ const app = express();
 
 const pubdirName = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(pubdirName));
 
+
 app.get('', (req, res) => {
-    res.send(index.html);
-});
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Kim Tae Hyung'
+    });
+})
 
 app.get('/about', (req, res) => {
-    res.render(about.html);
+    res.render('about',{
+        title: 'About Me',
+        name: 'BTS Logo'
+    });
 });
 
 app.get('/help', (req, res) =>{
-    res.send(help.html);
+    res.render('help', {
+        title: 'Help Page'
+    });
 })
 
 app.get('/weather', (req, res) => {
