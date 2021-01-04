a85fbc0bb00181ab88867bcddd91a8b5a312fa8c
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-01-04 18:56:21 +0530

    Render Geocode

---
+++ web-server/src/utils/geocode.js
@@ -0,0 +1,25 @@
+const { builtinModules } = require('module');
+const request = require('request');
+require('dotenv').config();
+
+const api_key = process.env.GEOCODE_API_KEY;
+
+const geocode = (address, callback) => {
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+address+".json?access_token="+api_key+"&limit=1";
+
+    request({url, json: true}, (error, { body }) => {
+        if(error){
+            callback( "Unable to connect", undefined);
+        } else if(body.features.length === 0){
+            callback("Unable to find location. Try another search", undefined);
+        }else {
+            callback(undefined, {
+                latitude : body.features[0].center[1],
+                longitude : body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode;
