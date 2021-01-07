e54478e9c36dbff614039a4af7b7a0f7447f7ce3
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-01-07 18:56:51 +0530

    Import User and Task (saving to db)

---
+++ task-manager/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+require('./db/mongoose')
+const User = require('./models/user');
+const Task = require('./models/task');
+const Tasks = require('./models/task');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body);
+
+    user.save().then(() => {
+        res.send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+});
+
+app.post('/tasks', (req, res) => {
+    const task = new Tasks(req.body);
+
+    task.save().then(() => {
+        res.send(task);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+})
+
+app.listen(port, () => {
+    console.log('Sever started on port '+port);
+});
