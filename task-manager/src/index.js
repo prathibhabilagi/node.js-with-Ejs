b36868793a1df0dc5ee316a068519de7ffe0559e
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-01-08 23:13:45 +0530

    Find and Update users and tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 require('./db/mongoose')
 const User = require('./models/user');
-const Task = require('./models/task');
 const Tasks = require('./models/task');
 
 const app = express();
@@ -17,6 +16,27 @@ app.post('/users', (req, res) => {
     })
 });
 
+app.get('/users', (req,res) => {
+    User.find({}).then((users) =>{
+        res.send(users);
+    }).catch((e) => {
+        res.status(500).send();
+    })
+});
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id).then((user) => {
+        if(!user) {
+            return res.status(400).send();
+        }
+        res.send(user);
+    }).catch((e) => {
+        res.status(500).send();
+    })
+});
+
 app.post('/tasks', (req, res) => {
     const task = new Tasks(req.body);
 
@@ -27,6 +47,27 @@ app.post('/tasks', (req, res) => {
     })
 })
 
+app.get('/tasks', (req, res) => {
+    Tasks.find().then((task) => {
+        res.send(task);
+    }).catch((e) => {
+        res.status(500).send();
+    })
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Tasks.findById(_id).then((task) => {
+        if(!task){
+            return res.status(404).send();
+        }
+        res.send(task);
+    }).catch((e) => {
+        res.status(500).send();
+    })
+})
+
 app.listen(port, () => {
     console.log('Sever started on port '+port);
-});
+}); 
