b8a00862e84537c33f8ddd9b1280b166fd692bad
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-01-12 19:03:56 +0530

    Using async and await to add, read, update and delete task

---
+++ task-manager/src/routers/task.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const Tasks = require('../models/task.js')
+const router = new express.Router()
+
+router.post('/tasks', async (req, res) => {
+    const task = new Tasks(req.body);
+
+    try{
+        const task = await task.save();
+        res.send(task);
+    }catch(e){
+        res.status(400).send(e);
+    }
+});
+
+
+router.get('/tasks', async (req, res) => {
+    
+    try{
+        const tasks = await Tasks.find();
+        res.send(tasks);
+    }catch(e){
+        res.status(500).send(e);
+    } 
+});
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try{
+        const task = await Tasks.findById(_id);
+        res.send(task);
+    }catch(e){
+        res.status(500).send(e);
+    }
+});
+
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation){
+        return res.status(400).send({error: 'Invalid updates!'});
+    }
+    
+    try{
+        const task = await Tasks.findById(req.params.id);
+        
+        updates.forEach((update) => task[update] = req.body[update]);
+        await task.save();
+        if(!Tasks){
+            return res.status(404).send();
+        }
+        res.send(task)
+    }catch(e){
+        res.status(500).send(e);
+    }
+});
+
+router.delete('/tasks/:id', async (req, res) => {
+    try{
+        const task = await Tasks.findByIdAndDelete(req.params.id);
+
+        if(!task){
+            return res.status(404).send()
+        }
+
+        res.send(task);
+    }catch(e){
+        return res.status(404).send(e);
+    }
+})
+
+module.exports = router;
