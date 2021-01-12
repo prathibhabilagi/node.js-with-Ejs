7dca65131d152260896b838d00c48886494a1b5a
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-01-12 18:46:43 +0530

    Making use of user Schema

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,55 +1,83 @@
 const mongoose = require('mongoose');
 const { default: validator } = require('validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if(!validator.isEmail(value)){
-                throw new Error('Email is invalid');
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if(!validator.isEmail(value)){
+                    throw new Error('Email is invalid');
+                }
             }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if(value.toLowerCase().includes('password')) {
-                throw new Error('Password should contain more than 6 characters');
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 7,
+            trim: true,
+            validate(value) {
+                if(value.toLowerCase().includes('password')) {
+                    throw new Error('Password should contain more than 6 characters');
+                }
             }
-        }
-    },
-    age: {
-        type: Number,
-        default: true,
-        validate(value) {
-            if(value < 0){
-                throw new Error('Age must be postive number');
+        },  
+        age: {
+            type: Number,
+            default: true,
+            validate(value) {
+                if(value < 0){
+                    throw new Error('Age must be postive number');
+                }
             }
-        }
+        },
+        tokens: [{
+            token: {
+                type: String,
+                required: true
+            }
+        }]
+    });
+userSchema.methods.generateAuthToken = async function() {
+    const user = this
+    const token = jwt.sign({_id: user._id.toString() }, 'thisisasecrete');
+    user.tokens = user.tokens.concat({ token })
+    await user.save();
+    return token
+}
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({email});
+    if(!user){
+        throw new Error('Unable to login');
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        throw new Error('Unable to login');
+    }
+    return user
+}
+
+userSchema.pre('save', async function(next) {
+    const user = this
+
+    if(user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next();
 })
 
-const me = new User({
-    name: 'Bangieeee',
-    email: 'btssss@army.co',
-    password: 'btsbtsbg',
-    age: 30
-})
-
-me.save().then(() =>{
-    console.log(me);
-}).catch((error) =>{
-    console.log('Error', error);
-});
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
