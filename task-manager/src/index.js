370cae40a5b8e76e6e9fbcbcde1a59a5064b3117
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-01-20 19:08:57 +0530

    Connects to the server

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -5,7 +5,7 @@ const taskRouter = require('./routers/task.js')
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use(userRouter);
@@ -14,15 +14,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Sever started on port '+port);
 }); 
-
-const jwt = require('jsonwebtoken');
-
-const myFunction = async() =>{
-    const token = jwt.sign({_id: 'abcd123'}, 'btsipurpleyou', {expiresIn: '7 days'});
-    console.log(token);
-
-    const data = jwt.verify(token, 'btsipurpleyou');
-    console.log(data);
-}
-
-myFunction();
