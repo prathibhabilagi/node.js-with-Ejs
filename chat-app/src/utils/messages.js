b22870be9648b9d58aa8aebb0b139766905c761f
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-01-25 19:08:39 +0530

    Generate current time when message and location sent

---
+++ chat-app/src/utils/messages.js
@@ -0,0 +1,18 @@
+const genearteMessage = (text) => {
+    return {
+        text,
+        createdAt: new Date().getTime()
+    }
+}
+
+const generateLocation = (url) => {
+    return {
+        url,
+        createdAt: new Date().getTime()
+    }
+}
+
+module.exports = {
+    genearteMessage, 
+    generateLocation
+}
