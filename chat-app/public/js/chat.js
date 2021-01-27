52811484a73e85bccd365deff6ba7bba81622304
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-01-27 12:11:53 +0530

    Delete chat-app directory

--- chat-app/public/js/chat.js
+++
@@ -1,9 +0,0 @@
-const socket = io()
-
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated', count)
-})
-
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('Clicked');
-})
